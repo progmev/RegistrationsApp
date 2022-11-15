6e7924218e36a21664b57137d5c22e1e18ee9010
Martynov Evgeny <martynov@wimix.by>
2022-11-15 19:16:26 +0300

    Project created

---
+++ RegistrationsApp/RegistrationsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RegistrationsApp
+//
+//  Created by Martynov Evgeny on 15.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
